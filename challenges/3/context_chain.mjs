0856d26345c3c9474356a4dd0e0198219f73c295
Shubham <shub_patel2295@yahoo.com>
2022-08-26 22:15:05 -0600

    completed all test cases

---
+++ challenges/3/context_chain.mjs
@@ -0,0 +1,44 @@
+// —————————————————————————————————————————————————————————————————————————————
+// Challenge: Context chain
+// —————————————————————————————————————————————————————————————————————————————
+// Complete the function `createContext`.
+//
+// The function must support two signatures:
+//
+//   function createContext(data?: object): Context;
+//   function createContext(parent?: Context, data?: object): Context;
+//
+// —————————————————————————————————————————————————————————————————————————————
+// Hint: There must be some kind of inheritance in JavaScript…
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create
+// —————————————————————————————————————————————————————————————————————————————
+
+/**
+ * @typedef {object} Context
+ */
+
+/**
+ * Creates a new context or inherits a parent one.
+ *
+ * If no parent context is specified, the function must return a context
+ * object.
+ *
+ * If a parent context is specified, the function must return a child context
+ * object. The child context gives access to its parent properties.
+ *
+ * Optionally, `data` can specified and merged into this context.
+ *
+ * @param {Context|object} [parentOrData] An optional parent context or data
+ * @param {object} [data] An optional set of properties to define
+ * @returns {Context} The child context
+ */
+export function createContext(parentOrData, data) {
+  if (data) {
+    let ctx = Object.create(parentOrData);
+    ctx = Object.assign(ctx, data);
+    return ctx;
+  }
+  else {
+    return Object.create(parentOrData);
+  }
+}
